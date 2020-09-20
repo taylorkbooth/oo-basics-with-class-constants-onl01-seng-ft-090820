197f16ae4ec16317e708291af2866af39f471952
Taylor Booth <taylorkbooth@gmail.com>
2020-09-20 01:09:50 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,7 +2,7 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
-  BRANDS = []
+  BRANDS = [Uggs]
 
   def initialize(brand)
     @brand = brand
@@ -18,6 +18,5 @@ class Shoe
     BRANDS << brands
   end
 
-  
 
 end
