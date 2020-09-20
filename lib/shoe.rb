b50818081723a028239672e280eb63761bbb993a
Taylor Booth <taylorkbooth@gmail.com>
2020-09-20 01:05:55 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,7 +2,7 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
-BRANDS= []
+  BRANDS = []
 
   def initialize(brand)
     @brand = brand
@@ -12,5 +12,7 @@ BRANDS= []
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
+  
+  
 
 end
